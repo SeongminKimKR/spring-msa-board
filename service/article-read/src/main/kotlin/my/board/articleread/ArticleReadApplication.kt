78ebcb3262c5431a75bc5af141d203446cbb9ede
SeongminKimKR <gvf156@gmail.com>
2025-04-05 11:20:07 +0900

    feat: 프로젝트 세팅

---
+++ service/article-read/src/main/kotlin/my/board/articleread/ArticleReadApplication.kt
@@ -0,0 +1,11 @@
+package my.board.articleread
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArticleReadApplication
+
+fun main(args: Array<String>) {
+    runApplication<ArticleReadApplication>(*args)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+rootProject.name = "spring-msa-board"
+
+include("common")
+include("service")
+include("service:article")
+include("service:comment")
+include("service:view")
+include("service:like")
+include("service:hot-article")
+include("service:article-read")
