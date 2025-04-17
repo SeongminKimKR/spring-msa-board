76cfbfea457f32171537459cb0f83e62c7383db9
SeongminKimKR <gvf156@gmail.com>
2025-04-17 23:32:55 +0900

    feat: 댓글 무한 depth 엔티티 설계

---
+++ service/comment/src/main/kotlin/my/board/comment/entity/CommentPath.kt
@@ -0,0 +1,73 @@
+package my.board.comment.entity
+
+import jakarta.persistence.Embeddable
+
+@Embeddable
+class CommentPath(
+    val path: String,
+) {
+
+    fun getDepth() = calDepth(path)
+
+    fun isRoot() = calDepth(path) == 1
+
+    fun getParentPath() = path.substring(0, path.length - DEPTH_CHUNK_SIZE)
+
+    fun createChildCommentPath(descendantsTopPath: String?): CommentPath {
+        if (descendantsTopPath == null) {
+            return from(path + MIN_CHUNK)
+        }
+        val childrenTopPath = findChildrenTopPath(descendantsTopPath)
+        return from(increase(childrenTopPath))
+    }
+
+    private fun findChildrenTopPath(descendantsTopPath: String): String =
+        descendantsTopPath.substring(0, (getDepth() + 1) * DEPTH_CHUNK_SIZE)
+
+    private fun increase(childrenTopPath: String): String {
+        // 00000 00000
+        val lastChunk = path.substring(path.length - DEPTH_CHUNK_SIZE)
+
+        check(isChunkOverflowed(lastChunk)) { "chunk overflowed" }
+
+        val charsetLength = CHARSET.length
+        var value = 0
+
+        for (ch in lastChunk.toCharArray()) {
+             value = value * charsetLength + CHARSET.indexOf(ch)
+        }
+
+        value += 1
+
+        var result = ""
+
+        for(i in 0..DEPTH_CHUNK_SIZE) {
+            result = CHARSET[value % charsetLength] + result
+            value /= charsetLength
+        }
+
+        return path.substring(0, path.length - DEPTH_CHUNK_SIZE) + result
+    }
+
+    private fun isChunkOverflowed(lastChunk: String) = MAX_CHUNK.equals(lastChunk)
+
+    companion object {
+        private const val CHARSET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstvwxyz"
+
+        private const val DEPTH_CHUNK_SIZE = 5
+        private const val MAX_DEPTH = 5
+
+        private val MIN_CHUNK = CHARSET[0].toString().repeat(DEPTH_CHUNK_SIZE)
+        private val MAX_CHUNK = CHARSET[CHARSET.length - 1].toString().repeat(DEPTH_CHUNK_SIZE)
+
+        fun from(path: String): CommentPath {
+            check(isDepthOverflowed(path)) { "depth overflowed" }
+
+            return CommentPath(path)
+        }
+
+        private fun isDepthOverflowed(path: String) = calDepth(path) > MAX_DEPTH
+
+        private fun calDepth(path: String): Int = path.length / DEPTH_CHUNK_SIZE
+    }
+}
---
+++ service/comment/src/main/kotlin/my/board/comment/entity/CommentV2.kt
@@ -0,0 +1,47 @@
+package my.board.comment.entity
+
+import jakarta.persistence.Embedded
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import my.board.comment.service.request.CommentCreateRequest
+import java.time.LocalDateTime
+
+@Table(name = "comment_v2")
+@Entity
+class CommentV2(
+    @Id
+    val commentId: Long,
+    val content: String,
+    @Embedded
+    val path: CommentPath,
+    val articleId: Long,
+    val writerId: Long,
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+) {
+    val parentCommentId: Long = parentCommentId ?: commentId
+
+    var deleted: Boolean = false
+        protected set
+
+    fun isRoot() = parentCommentId == commentId
+
+    fun delete() {
+        deleted = true
+    }
+
+    companion object {
+        fun from(
+            id: Long,
+            parentCommentId: Long?,
+            request: CommentCreateRequest,
+        ) = Comment(
+            commentId = id,
+            content = request.content,
+            parentCommentId = parentCommentId ?: id,
+            articleId = request.articleId,
+            writerId = request.writerId,
+        )
+    }
+}
+
